3d01f16f6afd6375a10cb1e40d8c7b7f806e9666
Reclocer <dzeger92@gmail.com>
2022-04-03 21:51:30 +0300

    [refact]  WinCellCount.cs

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -28,53 +28,7 @@ namespace Corebin.GravityX0
                 IsFull = true;
             }
         }
-    }
-
-    public class WinCellCount
-    {
-        public int XTiles;
-        public int OTiles;
-        public bool WinCombination;
-
-        private int _countingLimit;
-
-        public event Action<Fill> OnWin;
-                
-        public WinCellCount(int countingLimit)
-        {
-            _countingLimit = countingLimit;
-        }
-
-        public void IncrementXTiles()
-        {
-            OTiles = 0;
-            XTiles++;
-
-            if (XTiles == _countingLimit)
-            {
-                WinCombination = true;
-                OnWin(Fill.Iks);
-            }
-        }
-
-        public void IncrementOTiles()
-        {
-            XTiles = 0;
-            OTiles++;
-
-            if (OTiles == _countingLimit)
-            {
-                WinCombination = true;
-                OnWin(Fill.Zero);
-            }
-        }
-
-        public void Revert()
-        {
-            XTiles = 0;
-            OTiles = 0;
-        }
-    }
+    }    
 
     public class Board : MonoBehaviour
     {    
---
+++ Assets/Scripts/WinCellCount.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Corebin.GravityX0
+{
+    public class WinCellCount
+    {
+        public int XTiles;
+        public int OTiles;
+        public bool WinCombination;
+
+        private int _countingLimit;
+
+        public event Action<Fill> OnWin;
+
+        public WinCellCount(int countingLimit)
+        {
+            _countingLimit = countingLimit;
+        }
+
+        public void IncrementXTiles()
+        {
+            OTiles = 0;
+            XTiles++;
+
+            if (XTiles == _countingLimit)
+            {
+                WinCombination = true;
+                OnWin(Fill.Iks);
+            }
+        }
+
+        public void IncrementOTiles()
+        {
+            XTiles = 0;
+            OTiles++;
+
+            if (OTiles == _countingLimit)
+            {
+                WinCombination = true;
+                OnWin(Fill.Zero);
+            }
+        }
+
+        public void Revert()
+        {
+            XTiles = 0;
+            OTiles = 0;
+        }
+    }
+}
